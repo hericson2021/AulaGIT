fb9ad81212143d53b34f4ffbb6b14b31ca46b7e1
hericson araujo <hericson.araujo@convenio.uece.br>
2021-07-01 20:24:28 -0300

    Adicionar um print de teste

--- src/aulagit/AulaGIT.java
+++ src/aulagit/AulaGIT.java
@@ -15,7 +15,7 @@ public class AulaGIT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Estou testando o github");
     }
     
 }
